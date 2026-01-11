8bfb89c6bc39dedd81a084efafec713453975a8a
Riddhi Nagarbandhara <riddhinagar45@gmail.com>
2026-01-11 16:01:36 -0500

    variable added

--- variables.tf
+++ variables.tf
@@ -2,11 +2,13 @@ variable "github_token" {
   description = "GitHub personal access token"
   type        = string
   sensitive   = true
+  default = "${secret.Terraform_token}"
 }
 
 variable "github_owner" {
   description = "GitHub organization or user"
   type        = string
+  default = "Riddhi"
 }
 
 variable "repository_name" {
