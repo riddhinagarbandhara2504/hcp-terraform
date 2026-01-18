fe1a51c09cad9e9adb99d2fa6d8aa6a15c4f2df4
Riddhi Nagarbandhara <riddhinagar45@gmail.com>
2026-01-18 13:55:52 -0500

    update

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 
-# resource "github_repository" "repos" {
-#   for_each               = var.repositories
-#   name                   = each.value
-#   delete_branch_on_merge = true
-# }
+resource "github_repository" "repos" {
+  for_each               = var.repositories
+  name                   = each.value
+  delete_branch_on_merge = true
+}
 
 resource "github_repository_ruleset" "branch_naming" {
-  for_each    = var.repositories
+  # for_each    = var.repositories
   name        = "jira-branch-naming"
   repository  = github_repository.repos[each.key].name
   target      = "branch"
